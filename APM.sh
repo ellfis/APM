956797835fd49e2f184731a8d49d42b55fb52eba
jwb4209 <33587882+jwb4209@users.noreply.github.com>
2019-02-28 15:25:15 -0800

    Add files via upload

--- APM.sh
+++ APM.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
+ps1 () {
+        cat $2 |grep $1 | head -1 | sed 's/  */ /g' | cut -f 3,4 -d " " >> $1_cpu_mem.txt
+}
+
 # Monitors processes
-./APM1 127.0.0.1 &
-./APM2 127.0.0.1 &
-./APM3 127.0.0.1 &
-./APM4 127.0.0.1 &
-./APM5 127.0.0.1 &
-./APM6 127.0.0.1 &
+./APM1 192.168.1.141 &
+./APM2 192.168.1.141 &
+./APM3 192.168.1.141 &
+./APM4 192.168.1.141 &
+./APM5 192.168.1.141 &
+./APM6 192.168.1.141 &
+
+
 
 ps | grep APM > tempproclist.txt
 
@@ -28,8 +34,21 @@ metaprockill () {
 }
 
 trap metaprockill EXIT
+directory="/home/student/data"
 while [ 0 ]; do
-	sleep 60 #replace this with actual monitor code
+	sleep 5
+
+        ifstat ens33 | tail -2 | head -1 | sed 's/  */ /g' | cut -f 6,8 -d " " >> RX_TX_data.txt
+        ps -aux >> hold.txt
+        ps1 APM1 hold.txt
+        ps1 APM2 hold.txt
+        ps1 APM3 hold.txt
+        ps1 APM4 hold.txt
+        ps1 APM5 hold.txt
+        ps1 APM6 hold.txt
+        iostat | grep sda | sed 's/  */ /g' | cut -f 4 -d " " >> sda_writeKB.txt
+        df /dev/mapper/centos-root | tail -1 | sed 's/  */ /g' | cut -f 5 -d " " >> Dard_disk_utl.txt   
+
 done
 
 
